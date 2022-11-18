694a57577049243b6281f3d9610e544bf24e674e
christenis98 <canei01@ucm.es>
2022-11-18 16:40:13 +0100

    Not finished

---
+++ Katas/KataBingo/KataBingo/Card.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace KataBingo
+{
+    class Card
+    {
+        int[,] card = new int[5, 5];
+
+        
+     
+        /*List<int> list2 = new List<int>() { 16, 17,18 ,19, 20, 21 ,22, 23, 24 ,25, 26, 27 ,28 ,28, 30 };
+        List<int> list3 = new List<int>() { 31,32,33,34,35,36,37,38,39,40,41,42,43,44,45 };
+        List<int> list4 = new List<int>() { 46,47,48,49,50,51,52,53,54,55,56,57,58,59,60 };
+        List<int> list5 = new List<int>() { 61,62,63,64,65,66,67,68,69,70,71,72,73,74,75 };
+        */
+
+
+        public void generateCard()
+        {
+            List<int> list1 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            var rnd = new Random();
+            
+            
+            for (int column = 0; column < 5; column++)
+            {
+                var randomized = list1.OrderBy(item => rnd.Next()).ToList();
+
+
+
+                for (int row = 0; row < 5; row++)
+                {
+                    if (column == 2 && row == 2)
+                        card[row, column] = 0;
+                    else
+                    {
+                        card[column, row] = randomized[row];
+                    }
+
+                    Console.Write(card[column, row]);
+                }
+
+                for (int i = 1; i <= 15; i++)
+                {
+                    list1[i] += 15;
+                }
+                Console.Write("\n");
+            }
+
+        }        
+    }
+}
---
+++ Katas/KataBingo/KataBingo/Program.cs
@@ -0,0 +1,42 @@
+﻿
+NumbersGenerator();
+
+
+void NumbersGenerator()
+{
+    List<int> numbers=new List<int>();
+    int totalNumbers = 75;
+    
+    for (int i=1;i<=totalNumbers;i++)
+    {
+        numbers.Add(i);
+    }
+
+    var rnd = new Random();
+    var randomized = numbers.OrderBy(item => rnd.Next());
+
+    foreach (int number in randomized)
+    {
+        Console.WriteLine(number);             
+    }
+
+/*
+    Random rd = new Random();
+    int rn;
+    while (numbers.Count > 1)
+    {
+        rn = rd.Next(0, numbers.Count - 1);
+        numbers.RemoveAt(rn);
+        Console.WriteLine("Random Number: "+rn + " Bingo: " + numbers[rn] + " count: " + numbers.Count + "\n");
+        
+        for (int i = 0; i < numbers.Count; i++)
+        {
+
+            Console.Write(numbers[i] + ",");
+        }
+    }
+*/
+
+}
+
+
