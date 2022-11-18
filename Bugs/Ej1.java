882ceb170941c41d2d4dec03743e711b07c85c65
christenis98 <canei01@ucm.es>
2022-11-18 16:47:50 +0100

    Create Ej1.java

---
+++ Bugs/Ej1.java
@@ -0,0 +1,16 @@
+package codes;
+
+public class Ej1 {
+
+	public static void main(String[] args) {
+		
+		
+		private static byte char64(char x) {
+			  if ( /*Siempre sera falso porque char en java no tiene signo*/(int)x < 0 || (int)x > index_64.length)
+			    return -1;
+			  return index_64[(int)x];
+			}
+
+	}
+
+}
