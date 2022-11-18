bc6f0f6302b2af50d21a75be9b5a9fef0d3b100a
christenis98 <canei01@ucm.es>
2022-11-18 15:14:47 +0100

    KataFizzBuzz

---
+++ Katas/KataFizzBuzz/Back.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataFizzBuzz
+{
+    public class Back
+    {
+        public void FizzBuzz(int value)
+        {
+            /* var myList = new Dictionary<int, string>();
+
+             myList.Add(3, "fizz ");
+             myList.Add(5, "buzz ");
+             myList.Add(7, "pop ");
+             */
+            string result = "";
+            
+                        if (value % 3 == 0)
+                            result += "fizz ";
+                        if (value % 5 == 0)
+                            result += "buzz ";
+                        if (value % 7 == 0)
+                            result += "pop ";
+
+                        if (result == "")
+                            result = value.ToString();
+           
+
+           
+
+            Console.WriteLine(result);
+
+            
+
+        }
+    }
+}
---
+++ Katas/KataFizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using KataFizzBuzz;
+
+Start();
+
+void Start()
+{
+    bool finished = false;
+
+    while(!finished)
+    {
+        Back back = new Back();
+
+        Console.WriteLine("Insert a number:");
+        string number = Console.ReadLine();        
+        back.FizzBuzz(int.Parse(number));
+
+        Console.WriteLine("Want to play again?(Y/N)");
+        string answer = Console.ReadLine();
+        if(answer=="y") finished = false;    
+        else if(answer=="n") finished = true;
+    }
+}
+
+
