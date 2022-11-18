cc153072ab9827f7e24a2833ae0049386532e76f
christenis98 <canei01@ucm.es>
2022-11-18 16:53:31 +0100

    BUGSS

---
+++ Bugs/Problem2.java
@@ -0,0 +1,15 @@
+package codes;
+
+public class Problem2 {
+
+	public static void main(String[] args) {
+		
+		
+		public double[] computeMean() {
+			/*Si el tamaño es 1, la posicion de get(1) estara fuera de los limites del array, lo correcto seria get(0)*/
+			return points.size() == 1 ? points.get(1) : null;
+		}
+
+	}
+
+}
